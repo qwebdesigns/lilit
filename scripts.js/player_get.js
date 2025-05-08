d44830a36b0688935fbd852ee8437c639d6ab286
Qweb Design <qwebdesing@inbox.ru>
2025-05-08 15:52:40 +0300

    Update player_get.js

--- scripts.js/player_get.js
+++ scripts.js/player_get.js
@@ -1,6 +1,6 @@
 function fetchPlayerData(link) {
   fetch(
-    `http://77.222.58.245/php/player_get_bot.php?link=${encodeURIComponent(
+    `https://cors-anywhere.herokuapp.com/http://://77.222.58.245/php/player_get_bot.php?link=${encodeURIComponent(
       link
     )}`
   )
